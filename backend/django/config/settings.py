8ae991b3a15cee11eb9ea8c39feffed1694f74aa
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-02-14 19:45:39 -0300

    Por favor não dar erro

--- backend/django/config/settings.py
+++ backend/django/config/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
