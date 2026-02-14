b5669b9ee9191d3e7f483f8e84e0c98e6c204ef8
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-02-14 20:15:24 -0300

    Tentando DENOVO

--- backend/django/config/settings.py
+++ backend/django/config/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
+import os
+
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['.onrender.com']
-DEBUG = config('DEBUG')
+DEBUG = os.environ('DEBUG')
 
 
 # Application definition
@@ -82,8 +85,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-import os
 
 DATABASES = {
     'default': {
