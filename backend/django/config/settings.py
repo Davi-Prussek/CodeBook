073df174846636bfe8dc9ff568dc25e1612620e8
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-02-14 19:03:01 -0300

    ODIO DESSE DESGRAÇADO DE RENDER

--- backend/django/config/settings.py
+++ backend/django/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['.orender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 DEBUG = config('DEBUG')
 
 
